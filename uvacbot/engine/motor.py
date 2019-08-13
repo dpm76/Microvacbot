dfbcc8b126f2441a0379a7de40a982ebc2f42a18
David <info@davidpm.eu>
2019-08-13 17:10:16 +0200

    Engine driver and comments

--- uvacbot/engine/motor.py
+++ uvacbot/engine/motor.py
@@ -3,11 +3,25 @@ import pyb
 from userapp.uvacbot.signal.pwm import Pwm
 
 class Motor(object):
+    '''
+    Controls a motor
+    '''
+
 
     PWM_FREQ = 50.0
-    MAX_THROTTLE = 90.0
+    MIN_DUTY = 40.0
+    MAX_DUTY = 90.0
+    DIFF_DUTY = MAX_DUTY - MIN_DUTY
 
     def __init__(self, pwmPin, pwmTimer, pwmChannel, reversePin):
+        '''
+        Constructor
+        
+        @param pwmPin: Pin where the PWM-signal comes from
+        @param pwmTimer: Timer to produce the PWM signal
+        @param pwmChannel: Channel of the timer
+        @param reversePin: Pin which controls the reverse signal
+        '''
     
         self._pwm = Pwm(pwmPin, pwmTimer, pwmChannel, Motor.PWM_FREQ)
         self._reversePin = pyb.Pin(reversePin, pyb.Pin.OUT)
@@ -15,19 +29,43 @@ class Motor(object):
         
         
     def cleanup(self):
+        '''
+        Finishes and releases the resources
+        '''
     
         self._pwm.cleanup()
         self._reversePin.off()
         
         
     def setThrottle(self, throttle):
-    
-        if throttle > 0:       
-            self._reversePin.off()
-        else:
-            self._reversePin.on()
+        '''
+        Makes the motor spin
         
-        if -Motor.MAX_THROTTLE < throttle < Motor.MAX_THROTTLE: 
-            self._pwm.setDutyPerc(abs(throttle))
+        @param throttle: Percentage to spin the motor. This value can be negative, in that case, the motor spins reversed.
+        '''
+    
+        if throttle != 0:
+            if throttle > 0:       
+                self._reversePin.off()
+            else:
+                self._reversePin.on()
+            
+            modThrottle = abs(throttle)
+            if modThrottle > 100.0:
+                modThrottle = 100.0
+            
+            duty = MIN_DUTY + modThrottle * DIFF_DUTY / 100.0 
+            
+            self._pwm.setDutyPerc(duty)
+                
         else:
-            self._pwm.setDutyPerc(Motor.MAX_THROTTLE)
+        
+            self._pwm.setDutyPerc(0)
+            
+            
+    def stop(self):
+        '''
+        Stops the motor
+        '''
+        
+        self.setThrottle(0)
--- uvacbot/ui/heartbeat.py
+++ uvacbot/ui/heartbeat.py
@@ -1,30 +1,50 @@
 import uasyncio
 
 class Heartbeat(object):
+    '''
+    Control LED lightning to provide a basic user communication
+    '''
 
     class States(object):
+        '''
+        States
+        '''
     
         Waiting = 0
         Active  = 1
         
 
     def __init__(self, led):
+        '''
+        Constructor
+        
+        Board led the instance is using
+        '''
     
         self._led = led
         self._state = Heartbeat.States.Waiting
         
         
     def setState(self, state):
+        '''
+        Sets the state
+        '''
     
         self._state = state
         
         
     def getState(self):
+        '''
+        Gets the current state
+        '''
     
         return self._state
         
         
     async def run(self):
+        '''
+        Starts to light
+        '''
 
         self._led.off()
 
