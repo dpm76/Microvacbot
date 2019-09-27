95ffc83ce98edce19c1d90a7c7fda7227a26f863
David <info@davidpm.eu>
2019-09-27 19:45:40 +0200

    import order changed

--- playground/mpu6050.py
+++ playground/mpu6050.py
@@ -3,19 +3,18 @@ Created on 21 sept. 2019
 
 @author: David
 '''
+import pyb
 import sys
 sys.path.append("/flash/userapp")
 
-import pyb
-import uasyncio
-from math import degrees
 from uvacbot.sensor.mpu6050 import Mpu6050
-
+from uasyncio import sleep_ms, get_event_loop
+from math import degrees
 
 async def waitEnd():
     
     while not pyb.Switch().value():
-        await uasyncio.sleep_ms(200)
+        await sleep_ms(200)
         
 
 async def readMpu(mpu):
@@ -24,23 +23,21 @@ async def readMpu(mpu):
     while True:
         print("temp: {0:.3f}C".format(mpu.readTemperature()))
         print(["{0:.3f}".format(degrees(a)) for a in mpu.readAngles()])
-        await uasyncio.sleep_ms(500)
+        await sleep_ms(500)
             
         
 def main():
     
     print("MPU6050 test")
     mpu = Mpu6050(1)
-    
-    loop = uasyncio.get_event_loop()
-    
+        
+    loop = get_event_loop()
     try:
         loop.create_task(readMpu(mpu))
         loop.run_until_complete(waitEnd())
     
     finally:
         mpu.cleanup()
-    
 
 
 if __name__ == "__main__":
