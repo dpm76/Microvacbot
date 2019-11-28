7bef07d80b6d5655e4b65cbeed2f8f52235acb49
dpm76 <davidpm.itengineer@gmail.com>
2019-11-28 12:45:17 +0100

    Memory reduction for playground/smart_drive test

--- playground/smart_drive.py
+++ playground/smart_drive.py
@@ -1,11 +1,12 @@
-import sys
-import uasyncio
-sys.path.append("/flash/userapp")
+from sys import path
+path.append("/flash/userapp")
 
-import pyb
 from uvacbot.engine.motor import Motor
 from uvacbot.engine.driver import Driver, SmartDriver
 from uvacbot.sensor.mpu6050 import Mpu6050
+
+from uasyncio import sleep, get_event_loop
+from pyb import Pin
 
 LPF_ALPHA = 0.3
 
@@ -24,40 +25,40 @@ async def mainLoop(d):
         d.setMode(Driver.MODE_DRIVE)
         print("Driver @50")
         d.setMotionVector(50, 0)
-        await uasyncio.sleep(5)
+        await sleep(5)
         print("Driver @10")
         d.setMotionVector(10, 0)
-        await uasyncio.sleep(5)
+        await sleep(5)
         d.stop()
-        await uasyncio.sleep(1)
+        await sleep(1)
         print("Driver @-5")
         d.setMotionVector(-5, 0)
-        await uasyncio.sleep(3)
+        await sleep(3)
         d.stop()
-        await uasyncio.sleep(1)
+        await sleep(1)
         print("Turn @1")
         d.setMode(Driver.MODE_ROTATE)
         d.setMotionVector(0, 1)
-        await uasyncio.sleep(2)
+        await sleep(2)
         d.stop()
-        await uasyncio.sleep(1)
+        await sleep(1)
         d.setMode(Driver.MODE_DRIVE)
         print("Driver @5")
         d.setMotionVector(5, 0)
-        await uasyncio.sleep(5)
+        await sleep(5)
         d.stop()
     finally:
         d.cleanup()
         
 
 def main():
-    ml = Motor(pyb.Pin.board.D10, 4, 1, pyb.Pin.board.D11)
-    mr = Motor(pyb.Pin.board.D9, 8, 2, pyb.Pin.board.D8)
-    d = SmartDriver(ml, 3, pyb.Pin.board.D5, mr, 1, pyb.Pin.board.D7).setMotionSensor(Mpu6050(1))
+    ml = Motor(Pin.board.D10, 4, 1, Pin.board.D11)
+    mr = Motor(Pin.board.D9, 8, 2, Pin.board.D8)
+    d = SmartDriver(ml, 3, Pin.board.D5, mr, 1, Pin.board.D7).setMotionSensor(Mpu6050(1))
     d.setPidConstants([PID_KP]*2 + [PID_DIR_KP], [PID_KI]*2 + [PID_DIR_KI], [PID_KD]*2 + [PID_DIR_KD])
     d.setLpfAlphaConstant(LPF_ALPHA)
     
-    loop = uasyncio.get_event_loop()
+    loop = get_event_loop()
     loop.run_until_complete(mainLoop(d))
     loop.close()
     
