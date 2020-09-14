aebe221d58010451425ca515f2a0440a964a551f
dpm76 <davidpm.itengineer@gmail.com>
2020-09-14 15:14:54 +0200

    Functionality sketch in playground

---
+++ testing/playground/musical_instrument.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append("/flash/userapp")
+
+from pyb import Pin
+from uvacbot.sensor.ultrasound import Ultrasound
+from uvacbot.ui.buzzer import Buzzer
+
+from time import sleep_ms
+
+if __name__ == '__main__':
+
+    PLAY_TIME = 100 # ms
+
+    MIN_DIST = 10.0
+    MAX_DIST = 100.0
+    SPAN_DIST = MAX_DIST - MIN_DIST
+
+    MIN_FREQ = 220.0
+    MAX_FREQ = 880.0
+    SPAN_FREQ = MAX_FREQ - MIN_FREQ
+
+    meter = Ultrasound(Pin.board.D2, Pin.board.D4)
+    buzzer = Buzzer(Pin.board.D12, 3, 1)
+    
+    switch = Pin.board.SW    
+    offValue = switch.value()
+    
+    try:
+        
+        # Wait for switch button press the first time
+        print("Press switch button to start")
+        while switch.value() == offValue:
+            sleep_ms(100)
+    
+        # Wait for switch button release        
+        while switch.value() != offValue:
+            sleep_ms(100)
+    
+        # Wait for swith button press to finish
+        print("Press switch button to finish")
+        while switch.value() == offValue:
+            dist = meter.read()
+            if MIN_DIST <= dist <= MAX_DIST:
+                freq = MAX_FREQ - ((dist - MIN_DIST) * SPAN_FREQ / SPAN_DIST)
+                buzzer.buzz(freq, PLAY_TIME)
+            else:
+                sleep_ms(PLAY_TIME)
+            
+    finally:
+        meter.cleanup()
+        buzzer.cleanup()
