e5f046828d6630cdce37d935920217c918b54c19
David <info@davidpm.eu>
2021-06-18 11:07:09 +0200

    Turning playground tests modified.

--- testing/playground/turn_test.py
+++ testing/playground/turn_test.py
@@ -6,58 +6,43 @@ Created on 18-sep-2020
 
 import sys
 sys.path.append("/flash/userapp")
-
-from math import radians, pi, degrees
-
-from pyb import Pin
-from utime import sleep_ms
+from uvacbot.sensor.mpu6050 import Mpu6050
+from math import radians
+from micropython import alloc_emergency_exception_buf
+from pyb import Pin, Switch
+from uasyncio import run as uasyncio_run, sleep_ms
 from uvacbot.engine.driver import Driver
 from uvacbot.engine.motion import MotionController
 from uvacbot.engine.motor import Motor
-from uvacbot.modular_math import modularDiff
-from uvacbot.sensor.mpu6050 import Mpu6050
 
 
-def _readMpu(mpu):
-        
-        return mpu.readAngles()[2]
+alloc_emergency_exception_buf(100)
 
 
-def turn(motion, mpu, rads):
-        
-        maxdiff = radians(5)
-        pi2 = 2*pi
-        
-        curAngle = _readMpu(mpu)
-        diff = modularDiff(rads, curAngle, pi2)
-        if  diff > 0:
-                
-            print("R")
-            motion.turnRight()
-            
-        elif diff <= 0:
-                            
-            print("L")
-            motion.turnLeft()
-        
-        while abs(diff) > maxdiff:
-            print("{0:.3f}, {1:.3f}".format(degrees(curAngle), degrees(diff)), end='\t')
-            curAngle = _readMpu(mpu)
-            diff = abs(modularDiff(rads, curAngle, pi2))
-            sleep_ms(20)
-
-        
-        print("{0:.3f}, {1:.3f}".format(degrees(curAngle), degrees(diff)), end='\t')
-        print("OK")
-        motion.stop()
-        
-        #Check angle after stop
-        sleep_ms(1000)
-        curAngle = _readMpu(mpu)
-        print("Stoped at {0:.3f} with target {1}".format(degrees(curAngle), degrees(rads)))
-        
-        
+async def mainTask(motion):
+    
+    print("Press user switch to start.")
+    userSwitch = Switch()
+    while not userSwitch.value():
+        await sleep_ms(200)
+    
+    print("Starting")
+    await sleep_ms(1000)
+    
+    print("turning counter clockwise")
+    await motion.turn(radians(-30))
+    await sleep_ms(1000)
+    print("turning clockwise")
+    await motion.turn(radians(60))
+    print("finished")
+    
+    
 def main():
+    
+    print("Turn test")
+    
+    #20210618 DPM: The code of this example is configured for the NUCLEO-L746RG board.
+    #              Please, adapt according to the actual configuration.
     
     PID_KP = 250.0
     PID_KI = 0.0
@@ -72,13 +57,12 @@ def main():
     motorRight = Motor(Pin.board.D9, 8, 2, Pin.board.D8)
     motorDriver = Driver(motorLeft, motorRight)
     
-    motion = MotionController(mpu, motorDriver, PID_KP, PID_KI, PID_KD)
-    motion.setRotation(60)
-    
+    motion = MotionController(mpu, motorDriver, PID_KP, PID_KI, PID_KD)    
     
     try:
-        turn(motion, mpu, radians(-45))
+        uasyncio_run(mainTask(motion))
     finally:
+        motion.stop()
         mpu.cleanup()
         motorDriver.cleanup()
 
